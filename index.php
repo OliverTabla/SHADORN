def9576f96f9a632063ae51733b1e9822f948287
OliverTabla <oliver.tabla.prieto@ieselcalamot.com>
2025-05-11 15:41:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//Pagina en php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>SHADORN</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <iframe src="/wp-content/uploads/shadorn/index.html"></iframe>
+</body>
+</html>
